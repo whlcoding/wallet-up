aaacc5c7ced915e856e8b5d89c8cf0d097f23f53
whendel morais <whendel.morais.9@gmail.com>
2022-12-18 20:28:32 -0300

    feat: add auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+
+    /**
+     * logout
+     *
+     * @return JsonResponse
+     */
+    public function logout(): JsonResponse
+    {
+        Auth::user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logged out'
+        ], 200);
+    }
+
+    /**
+     * login
+     *
+     * @param  Request $args
+     * @return JsonResponse
+     */
+    public function login(Request $args): JsonResponse
+    {
+        $validation = Validator::make($args->only(['email', 'password']), [
+            'email' => ['required', 'email'],
+            'password' => ['required', 'string', 'max:245']
+        ]);
+
+        if ($validation->fails()) {
+            return response()->json([
+                'errors' => $validation->errors()->all()
+            ], 422);
+        }
+
+        $credentials = $args->only(['email', 'password']);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'errors' => ['Invalid credentials']
+            ], 401);
+        }
+
+        $token = Auth::user()->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token_access' => $token,
+            'token_type' => 'Bearer'
+        ], 200);
+    }
+
+    /**
+     * register
+     *
+     * @param  Request $args
+     * @return JsonResponse
+     */
+    public function register(Request $args): JsonResponse
+    {
+        $validation = Validator::make($args->only(['first_name', 'last_name', 'email', 'password']), [
+            'first_name' => ['required', 'string', 'max:245'],
+            'last_name' => ['required', 'string', 'max:245'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'string', 'max:245'],
+        ]);
+
+        if ($validation->fails()) {
+            return response()->json([
+                'errors' => $validation->errors()->all()
+            ], 422);
+        }
+
+        $user = User::create([
+            'first_name' => $args->first_name,
+            'last_name' => $args->last_name,
+            'email' => $args->email,
+            'password' => Hash::make($args->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'token_access' => $token,
+            'token_type' => 'Bearer'
+        ], 201);
+    }
+}
