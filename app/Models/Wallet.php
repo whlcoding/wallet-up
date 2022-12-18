e00702edc9ab1cd9920d062b2bcadf2ef0af9f14
whendel morais <whendel.morais.9@gmail.com>
2022-12-18 20:31:18 -0300

    feat: add wallet model, factory and seeder

---
+++ app/Models/Wallet.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Wallet extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name'];
+
+    // Relationships
+
+    /**
+     * user
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
