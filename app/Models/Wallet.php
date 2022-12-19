3b9788559870db434474ddccc99c955bb73d928d
whendel morais <whendel.morais.9@gmail.com>
2022-12-19 08:54:12 -0300

    feat: add transaction relationships

--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -5,13 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Wallet extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['name'];
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
 
     // Relationships
 
@@ -23,5 +32,25 @@ class Wallet extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    /**
+     * recurringTransactions
+     *
+     * @return HasMany
+     */
+    public function recurringTransactions(): HasMany
+    {
+        return $this->hasMany(RecurringTransaction::class, 'wallet_id', 'id');
+    }
+
+    /**
+     * transactions
+     *
+     * @return HasMany
+     */
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Wallet::class, 'wallet_id', 'id');
     }
 }
