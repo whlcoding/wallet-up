5095ceae9b919a2b8a39ec41a5886159b9404480
whendel morais <whendel.morais.9@gmail.com>
2023-04-06 21:07:25 -0300

    feat: add transaction crud methods on controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,33 +3,74 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\TransactionRequest;
+use App\Http\Requests\UpdateTransactionRequest;
+use App\Models\User;
 use App\Services\TransactionService;
-use App\Services\WalletService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
+    protected User $user;
+
+    protected TransactionService $transactionService;
+
+    public function __construct(TransactionService $transactionService)
+    {
+        $this->transactionService = $transactionService;
+
+        $this->middleware(function ($request, $next) {
+            $this->user = Auth::user();
+            return $next($request);
+        });
+    }
+
+
+    public function all(int $walletID): JsonResponse
+    {
+        $transactions = $this->transactionService->getAllTransactionsByWallet($this->user, $walletID);
+
+        return response()->json([
+            'message' => 'Transactions fetched successfully!',
+            'data' => $transactions
+        ], 200);
+    }
+
+    public function find(int $walletID, int $transactionId): JsonResponse
+    {
+        $transaction = $this->transactionService->getTransactionByWallet($this->user, $walletID, $transactionId);
+
+        return response()->json([
+            'message' => 'Transaction fetched successfully!',
+            'data' => $transaction
+        ], 200);
+    }
 
     public function store(TransactionRequest $transactionRequest): JsonResponse
     {
-        try {
-            $user = Auth::user();
-            $wallet = (new WalletService)->findWalletByUser($user, $transactionRequest->input('wallet_id'));
+        $transaction = $this->transactionService->createTransaction(
+            $transactionRequest->input('wallet_id'),
+            $transactionRequest->toArray()
+        );
 
-            // $dataTransaction = $transactionRequest->only()
+        return response()->json([
+            'message' => 'Transaction Created Successfully!',
+            'data' => $transaction
+        ], 201);
+    }
 
-            $transaction = (new TransactionService)->createTransaction($wallet, $transactionRequest->toArray());
+    public function update(UpdateTransactionRequest $transactionRequest): JsonResponse
+    {
+        $transaction = $this->transactionService->updateTransaction(
+            $this->user,
+            $transactionRequest->input('wallet_id'),
+            $transactionRequest->input('transaction_id'),
+            $transactionRequest->toArray()
+        );
 
-            return response()->json([
-                'message' => 'Transaction Created Successfully!',
-                'data' => $transaction
-            ], 201);
-        } catch (\Throwable $e) {
-            return response()->json([
-                'message' => 'Failed to create transaction!',
-                'error' => $e->getMessage()
-            ], $e->getCode() ?? 500);
-        }
+        return response()->json([
+            'message' => 'Transaction Updated Successfully!',
+            'data' => $transaction
+        ], );
     }
 }
